053c21b7f2615a32367cddaae9f8379be705daab
Justin Kuoch <justinkuoch257@gmail.com>
2024-12-06 13:26:28 -0800

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,37 +1,40 @@
-function search(){
-    navigateToPage('itemsearch.html');
-  }
-  
-  function newliststart(){
-    navigateToPage('newlist.html');
-  }
-  
-  function newlistmore(){
-    navigateToPage('otherhtmls/morelist.html');
-  }
-  
-  function vonstrip(){
-    navigateToPage('otherhtmls/vontrip.html');
-  }
-  
-  function walmarttrip(){
-    navigateToPage('otherhtmls/waltrip.html');
-  }
-  
-  function tripmore(){
-    navigateToPage('otherhtmls/moretrip.html');
-  }
-  
-  function targetsale(){
-    navigateToPage('otherhtmls/target.html');
-  }
-  
-  function ralphscoupon(){
-    navigateToPage('otherhtmls/ralphs.html');
-  }
-  
-  function sandcmore(){
-    navigateToPage('otherhtmls/sale.html');
-  }
+function navigateToPage(path) {
+  window.location.href = path;
+}
 
-  // update
+function search(){
+  navigateToPage('itemsearch.html');
+}
+  
+function newliststart(){
+  navigateToPage('newlist.html');
+}
+  
+function newlistmore(){
+  navigateToPage('otherhtmls/morelist.html');
+}
+  
+function vonstrip(){
+  navigateToPage('otherhtmls/vontrip.html');
+}
+  
+function walmarttrip(){
+  navigateToPage('otherhtmls/waltrip.html');
+}
+  
+function tripmore(){
+   navigateToPage('otherhtmls/moretrip.html');
+}
+  
+function targetsale(){
+  navigateToPage('otherhtmls/target.html');
+}
+  
+function ralphscoupon(){
+  navigateToPage('otherhtmls/ralphs.html');
+}
+  
+function sandcmore(){
+  navigateToPage('otherhtmls/sale.html');
+}
+
