9e21ec1a3e96e3a573105e9481fc23f03d408500
Justin Kuoch <justinkuoch257@gmail.com>
2024-12-06 16:28:45 -0800

    Item Search Anchor Update

--- itemsearch.js
+++ itemsearch.js
@@ -1,8 +1,3 @@
-function search() {
-    // Navigate to the next HTML file
-    window.location.href = 'orangejuice.html';
-}
-
 function goBack() {
     if (window.history.length > 1) {
         window.history.back(); // Go to the previous page in the history
